e63130b0c88dc4d508dbb1b4639cff57d710a137
milkchoco3524 <69533808+milkchoco3524@users.noreply.github.com>
2024-04-30 23:35:57 +0900

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,103 @@
+# text1 = "네이스123"
+# text2 = "123123"
+# text3 = "!@#$"
+# text4 = "!@#$1234"
+# text5 = "wtf"
+# text6 = "wtf123123"
+# text7 = "한글"
+# print(text1.isalnum())
+# print(text2.isalnum())
+# print(text3.isalnum())
+# print(text4.isalnum())
+# print(text5.isalnum())
+# print(text6.isalnum())
+# print(text7.isalnum())
+
+
+
+# text = "!#$$!@$!!!!!!!!@$#231421234네123^&^#$#이!#12313_+{스a"
+# text1 = "네이스"
+# text2 = "123123"
+# text3 = "!@#$"
+# check = ""
+# print(text1.isalpha())
+# print(text2.isalpha())
+# print(text3.isalpha())
+# for i in text:
+#     if i.isalpha():
+#         check += i
+# print(check)
+
+
+
+# a = "BlockDMask"    # 문자로만 이루어짐
+# b = "1234Blog"      # 문자 + 숫자
+# c = "1231231"       # 숫자
+# d = "-234"          # 음수
+# e = "1.23"          # 소수점
+# f = "3^2"           # 3의 2제곱 기호 숫자
+# g = '2/3'           # 수학 기호 숫자 2/3
+# h = "0"             # 0
+# i = "0123"
+# # 0 으로 시작한 숫자
+# # str.isdgit("문자열")
+# print(f"'{a}' .isdigit() : {str.isdigit(a)}")
+# print(f"'{b}' .isdigit() : {str.isdigit(b)}")
+# print(f"'{c}' .isdigit() : {str.isdigit(c)}")
+# print(f"'{d}' .isdigit() : {str.isdigit(d)}")
+# print(f"'{e}' .isdigit() : {str.isdigit(e)}")
+# print(f"'{f}' .isdigit() : {str.isdigit(f)}")
+# print(f"'{g}' .isdigit() : {str.isdigit(g)}")
+# print(f"'{h}' .isdigit() : {str.isdigit(h)}")
+# print(f"'{i}' .isdigit() : {str.isdigit(i)}")
+# print()
+
+
+
+# a = '12345678'
+# print(a.isdigit())
+# print(a.isdecimal())
+# print(a.isnumeric())
+
+
+# x = '3^2'
+# print(x)
+# print(x.isdigit())
+# # True
+# print(x.isdecimal())
+# # False
+# print(x.isnumeric())
+
+
+# a = 'I love Python'
+
+# print( a.islower() )
+# print( a.isupper() )
+# print( a.upper() )
+# print( a.lower() )
+
+
+# str = "THIS is string example....wow!!!"
+# print(str.islower())
+# print(str.isupper())
+
+# str = "this is string example....wow!!!"
+# print(str.islower())
+
+# str = "this is string example....wow!!!"
+# print(str.swapcase())
+
+# str = "THIS IS STRING EXAMPLE....WOW!!! 1234"
+# print(str.swapcase())
+
+
+# str = "This Is String Example....Wow!!!"
+# print( str.istitle() )
+
+# str = "This is string example....wow!!!"
+# print( str.istitle() )
+# print( str.title() )
+
+
+print( 'hello world!'.capitalize() ) # Hello world!
+print( 'HELLO WORLD!'.capitalize() ) # Hello world!
